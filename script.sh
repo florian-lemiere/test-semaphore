a92f906042500c736d40069cece480e0a01ceaae
florian-lemiere <lemiere.florian@gmail.com>
2025-09-05 09:04:18 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-curl https://power-plugins.com/api/flipsum/ipsum/lorem-ipsum?paragraphs=3&start_with_fixed=1
+TEXTE=$(curl https://power-plugins.com/api/flipsum/ipsum/lorem-ipsum?paragraphs=3&start_with_fixed=1)
+echo $TEXTE > /tmp/lorem.txt
